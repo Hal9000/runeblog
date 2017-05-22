a8f730a6cb82fa78e46907a0226f8301bce84493
Hal Fulton <rubyhacker@gmail.com>
2017-05-22 10:43:47 -0500

    catches up

--- lib/runeblog.rb
+++ lib/runeblog.rb
@@ -3,7 +3,7 @@ require 'yaml'
 require 'livetext'
 
 class RuneBlog
-  VERSION = "0.0.40"
+  VERSION = "0.0.41"
 
   Path  = File.expand_path(File.join(File.dirname(__FILE__)))
   DefaultData = Path + "/../data"
