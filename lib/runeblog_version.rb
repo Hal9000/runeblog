196059c402ac1b3955c01b566d6fcedfb2c22b2e
Hal Fulton <rubyhacker@gmail.com>
2019-08-06 16:42:10 -0500

    Catchup after rubygems bug

--- lib/runeblog_version.rb
+++ lib/runeblog_version.rb
@@ -2,7 +2,7 @@
 if ! (Object.constants.include?(:RuneBlog) && RuneBlog.constants.include?(:Path))
 
 class RuneBlog
-  VERSION = "0.1.77"
+  VERSION = "0.1.78"
 
   Path  = File.expand_path(File.join(File.dirname(__FILE__)))
 end
