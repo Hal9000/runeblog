6b3df7c246edf37d62fbc0aafbbb5c3f8b95ccfc
Hal Fulton <rubyhacker@gmail.com>
2019-10-15 12:12:26 -0500

    makes todo list an lt3

--- lib/runeblog_version.rb
+++ lib/runeblog_version.rb
@@ -2,7 +2,7 @@
 if ! (Object.constants.include?(:RuneBlog) && RuneBlog.constants.include?(:Path))
 
 class RuneBlog
-  VERSION = "0.2.40"
+  VERSION = "0.2.41"
 
   path = Gem.find_files("runeblog").grep(/runeblog-/).first
   Path  = File.dirname(path)
