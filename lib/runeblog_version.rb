62485887c18cb7cb086173edc9d0f181c93a573d
Hal Fulton <rubyhacker@gmail.com>
2019-10-29 14:36:49 -0500

    adds command line framework + rebuild/preview commands

--- lib/runeblog_version.rb
+++ lib/runeblog_version.rb
@@ -2,7 +2,7 @@
 if ! (Object.constants.include?(:RuneBlog) && RuneBlog.constants.include?(:Path))
 
 class RuneBlog
-  VERSION = "0.2.56"
+  VERSION = "0.2.57"
 
   path = Gem.find_files("runeblog").grep(/runeblog-/).first
   Path  = File.dirname(path)
