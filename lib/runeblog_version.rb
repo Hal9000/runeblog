d6cf7c5b9114007485f883265a11c6b6891189e9
Hal Fulton <rubyhacker@gmail.com>
2019-12-04 00:01:18 -0600

    fixes gemspec glitch

--- lib/runeblog_version.rb
+++ lib/runeblog_version.rb
@@ -2,7 +2,7 @@
 if ! (Object.constants.include?(:RuneBlog) && RuneBlog.constants.include?(:Path))
 
 class RuneBlog
-  VERSION = "0.2.67"
+  VERSION = "0.2.68"
 
   path = Gem.find_files("runeblog").grep(/runeblog-/).first
   Path  = File.dirname(path)
