b5a73dab45a7b97733c5cf558b47d0721df5d94c
Hal Fulton <rubyhacker@gmail.com>
2019-12-04 19:46:30 -0600

    no, seriously

--- lib/runeblog_version.rb
+++ lib/runeblog_version.rb
@@ -4,7 +4,7 @@ require 'pathmagic'
 if ! (Object.constants.include?(:RuneBlog) && RuneBlog.constants.include?(:Path))
 
 class RuneBlog
-  VERSION = "0.2.71"
+  VERSION = "0.2.72"
 
   path = Gem.find_files("runeblog").grep(/runeblog-/).first
   Path  = File.dirname(path)
