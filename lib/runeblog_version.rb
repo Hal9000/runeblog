a0b84721111afddfedd798761c815bcda7daf950
Hal Fulton <rubyhacker@gmail.com>
2019-12-04 22:53:57 -0600

    refixes dumb mistakes

--- lib/runeblog_version.rb
+++ lib/runeblog_version.rb
@@ -2,7 +2,7 @@
 if ! (Object.constants.include?(:RuneBlog) && RuneBlog.constants.include?(:Path))
 
 class RuneBlog
-  VERSION = "0.2.72"
+  VERSION = "0.2.73"
 
   path = Gem.find_files("runeblog").grep(/runeblog-/).first
   Path  = File.dirname(path)
