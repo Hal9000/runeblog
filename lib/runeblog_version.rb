a7a91ba19b2cfe19b31919d820d80bb34942455d
Hal Fulton <rubyhacker@gmail.com>
2020-02-14 22:49:09 -0600

    adds exceptions.rb

--- lib/runeblog_version.rb
+++ lib/runeblog_version.rb
@@ -1,7 +1,7 @@
 if !defined?(RuneBlog::Path)
 
 class RuneBlog
-  VERSION = "0.3.18"
+  VERSION = "0.3.19"
 
   path = Gem.find_files("runeblog").grep(/runeblog-/).first
   Path  = File.dirname(path)
