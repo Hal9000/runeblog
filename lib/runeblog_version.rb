1d86358e57b596dadfb96c5c5e69108c2df881ee
Hal Fulton <rubyhacker@gmail.com>
2020-12-07 22:46:13 -0600

    updates livetext gem version (oops)

--- lib/runeblog_version.rb
+++ lib/runeblog_version.rb
@@ -1,7 +1,7 @@
 if !defined?(RuneBlog::Path)
 
 class RuneBlog
-  VERSION = "0.3.24"
+  VERSION = "0.3.25"
 
   path = Gem.find_files("runeblog").grep(/runeblog-/).first
   Path  = File.dirname(path)
