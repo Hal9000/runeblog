3c287cf3c5d322d3bd1beffa2c8a2e88360c357a
Hal Fulton <rubyhacker@gmail.com>
2024-05-20 12:17:13 -0500

    bumps version, makes new gem

--- lib/runeblog_version.rb
+++ lib/runeblog_version.rb
@@ -3,7 +3,7 @@ if !defined?(RuneBlog::Path)
 require 'pathname'
 
 class RuneBlog
-  VERSION = "0.3.34"
+  VERSION = "0.3.35"
 
   path = Gem.find_files("runeblog").grep(/runeblog-/).first
   path ||= Pathname(__FILE__).realpath.dirname.to_s
